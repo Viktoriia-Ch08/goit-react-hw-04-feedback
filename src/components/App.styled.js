ff9cfa19cfee98541bd357e36c3373ae5216a710
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2023-10-04 15:00:38 +0300

    fixed styles

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
 
-const ContainerApp = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const Container = styled.div`
+  width: 650px;
+  min-height: 670px;
+  max-height: 1050px;
+  margin: 30px auto;
+  padding: 30px;
+
   font-size: 40px;
+
+  background-color: rgba(197, 144, 228, 0.8);
+  border-radius: 20px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px,
+    rgba(0, 0, 0, 0.22) 0px 10px 10px;
 `;
 
-export { ContainerApp };
+export { Container };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -7,7 +7,7 @@ const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
       <p>Total: {total}</p>
-      <p>Positive feedback: {positivePercentage} %</p>
+      <p>Positive feedbacks: {positivePercentage} %</p>
     </Wrapper>
   </>
 );
