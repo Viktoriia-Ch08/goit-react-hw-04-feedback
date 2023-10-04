7fd648e1330672da49c5d137153777e340f55b77
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2023-10-04 16:45:37 +0300

    small fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 
-export const App = () => {
+export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -58,4 +58,4 @@ export const App = () => {
       </Section>
     </Container>
   );
-};
+}
